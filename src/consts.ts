b19d61182b2954ebcead05252669351645c92240
elshiraphine <elsherviana@gmail.com>
2025-06-03 14:03:01 +0700

    feat: add first post
    pie time picoCTF challenge

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'E';
+export const SITE_DESCRIPTION = 'Welcome to my slightly chaotic corner of the web — part digital playground, part brain-dump. Expect a mix of half-baked ideas, side quests, experiments that may or may not compile, and the occasional stroke of genius (I hope). It’s all a work in progress... kind of like me.';
